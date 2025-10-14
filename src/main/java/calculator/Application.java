7e78b8a42d2157e7109577cb77d7c48dbfd3fca8
jhk01007 <jhk001007@naver.com>
2025-10-14 20:52:15 +0900

    feat: 커스텀 구분자 기능 구현
    - 커스텀 구분자는 문자열 앞 부분에 "//" "\n" 사이에 위치
    - 커스텀 구분자를 추가하는 입력인지 판별하기 위해 정규표현식 사용

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,24 +2,36 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
-    private static List<String> separator = Arrays.asList(",", ":");
+    private static final List<String> SEPARATOR_LIST = new ArrayList<>(List.of(",", ";"));
 
     public static void main(String[] args) {
         // 입력
         System.out.println("덧셈할 문자열을 입력해주세요.");
         String input = Console.readLine();
 
+        // 커스텀 구분자 추출
+        // 커스텀 구분자를 새로 지정하는 문자열인지(정규 표현식 활용)
+        if(input.matches("^//[^\\\\n]+\\\\n.*")) {
+            // 커스텀 구분자 추출
+            String customSeparator = input.substring(2, input.indexOf("\\n"));
+            SEPARATOR_LIST.add(customSeparator);
+
+            // 추출 후 커스텀 구분자 관련 부분은 제거
+            input = input.substring(input.indexOf("\\n") + 2);
+        }
+
+
         double total = 0;
 
         // 빈문자열이 아닌 경우
         if(!input.isBlank()) {
             // 각 구분자를 통일
-            for (String s : separator) {
+            for (String s : SEPARATOR_LIST) {
                 input = input.replace(s, " ");
             }
 
