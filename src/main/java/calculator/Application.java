a0cf71e6ea5f95ca05f74fcd176a88e724b0c5a8
jhk01007 <jhk001007@naver.com>
2025-10-14 17:30:10 +0900

    fix: 빈 문자열이 들어오는 경우 0이 들어오도록 수정
    - 기존 코드에서는 빈 문자열이 들어오면 예외 발생
    - 이를 0이 반환되도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -16,17 +16,20 @@ public class Application {
 
         double total = 0;
 
-        // 각 구분자를 통일
-        for (String s : separator) {
-            input = input.replace(s, " ");
-        }
+        // 빈문자열이 아닌 경우
+        if(!input.isBlank()) {
+            // 각 구분자를 통일
+            for (String s : separator) {
+                input = input.replace(s, " ");
+            }
 
-        // 숫자들만 뽑아냄
-        String[] numbers = input.split(" ");
+            // 숫자들만 뽑아냄
+            String[] numbers = input.split(" ");
 
-        // 타입변환 후 각 숫자의 합 계산
-        for (int i = 0; i < numbers.length; i++) {
-            total += Double.parseDouble(numbers[i]);
+            // 타입변환 후 각 숫자의 합 계산
+            for (int i = 0; i < numbers.length; i++) {
+                total += Double.parseDouble(numbers[i]);
+            }
         }
 
         // 출력
