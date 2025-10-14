a36cceff23b06e38e70b3b1eff6bb80fc4af9e09
jhk01007 <jhk001007@naver.com>
2025-10-14 17:27:29 +0900

    feat: 쉼표(,) 콜론(:) 구분자 기능 구현
    구분자가 쉼표, 콜론인 경우 해당 구분자를 기준으로 분리하여 각 숫자들을 더한 값을 출력함

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,35 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
+    private static List<String> separator = Arrays.asList(",", ":");
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 입력
+        System.out.println("덧셈할 문자열을 입력해주세요.");
+        String input = Console.readLine();
+
+        double total = 0;
+
+        // 각 구분자를 통일
+        for (String s : separator) {
+            input = input.replace(s, " ");
+        }
+
+        // 숫자들만 뽑아냄
+        String[] numbers = input.split(" ");
+
+        // 타입변환 후 각 숫자의 합 계산
+        for (int i = 0; i < numbers.length; i++) {
+            total += Double.parseDouble(numbers[i]);
+        }
+
+        // 출력
+        System.out.printf("결과 : %f", total);
     }
 }
