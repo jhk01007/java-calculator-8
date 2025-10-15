91a78c7c2781b494f52a3a7169c5e1bd3ae6efc8
jhk01007 <jhk001007@naver.com>
2025-10-15 14:50:17 +0900

    refactor: 메서드 분리
    - 커스텀 구분자를 추출하는 부분을 메서드로 분리
    - 뽑아낸 숫자가 담긴 문자열 형태의 리스트들의 각 요소를 숫자로 변환하는 부분을 메서드로 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Application {
 
     private static final List<String> SEPARATOR_LIST = new ArrayList<>(List.of(",", ";")); // 콤마와 콜론은 미리 초기화
+    public static final String CUSTOM_SEPARATOR_REGEX = "^//[^\\\\n]+\\\\n.*"; // 커스텀 구분자 정규표현식
 
     public static void main(String[] args) {
         // 입력
@@ -16,30 +17,14 @@ public class Application {
 
         // 커스텀 구분자 추출
         // 커스텀 구분자를 새로 지정하는 문자열인지(정규 표현식 활용)
-        if (input.matches("^//[^\\\\n]+\\\\n.*")) {
-            // 커스텀 구분자 추출
-            String customSeparator = input.substring(2, input.indexOf("\\n"));
+        if (input.matches(CUSTOM_SEPARATOR_REGEX)) {
+            String customSeparator = extractCustomSeparator(input); // 커스텀 구분자 추출
+            SEPARATOR_LIST.add(customSeparator); // 커스텀 구분자 추가
 
-            // 커스텀 구분자가 숫자인지 검증
-            try {
-                double value = Double.parseDouble(customSeparator);
-                // 변환이 성공하면 예외 발생
-                throw new IllegalArgumentException("커스텀 구분자로 숫자는 사용할 수 없습니다.");
-            } catch (NumberFormatException ignored) {
-            }
-
-            // 커스텀 구분자가 마침표(.)인지 검증
-            if (customSeparator.equals(".")) {
-                throw new IllegalArgumentException("커스텀 구분자로 마침표(.)은 사용할 수 없습니다.");
-            }
-
-            SEPARATOR_LIST.add(customSeparator);
-
-            // 추출 후 커스텀 구분자 관련 부분은 제거
-            input = input.substring(input.indexOf("\\n") + 2);
+            input = input.substring(input.indexOf("\\n") + 2); // 추출 후 커스텀 구분자 관련 부분은 제거
         }
 
-
+        // 숫자 계산
         double total = 0;
 
         // 빈문자열이 아닌 경우
@@ -52,25 +37,51 @@ public class Application {
             // 숫자들만 뽑아냄
             String[] numbers = input.split(" ");
 
-            // 모든 요소들이 지정된 구분자외에 다른 구분자이거나 양수가 아닌지 검증
             for (int i = 0; i < numbers.length; i++) {
-                try {
-                    double num = Double.parseDouble(numbers[i]);
-                    if (num <= 0) {
-                        throw new IllegalArgumentException("양수만 가능합니다.");
-                    }
-                } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("지정된 구분자외에 다른 구분자는 사용할 수 없습니다.");
-                }
-            }
-
-            // 타입변환 후 각 숫자의 합 계산
-            for (int i = 0; i < numbers.length; i++) {
-                total += Double.parseDouble(numbers[i]);
+                // 문자열을 숫자로 변환
+                total += convertStringToNumber(numbers[i]);
             }
         }
 
         // 출력
         System.out.printf("결과 : %f", total);
+    }
+
+    private static String extractCustomSeparator(String input) {
+        String customSeparator = input.substring(2, input.indexOf("\\n")); // 커스텀 구분자 추출
+
+        isCustomSeparatorNumber(customSeparator); // 커스텀 구분자가 숫자인지 검증
+        isCustomSeparatorDot(customSeparator); // 커스텀 구분자가 마침표(.)인지 검증
+
+        return customSeparator;
+    }
+
+    private static double convertStringToNumber(String numbers) {
+        try {
+            double num = Double.parseDouble(numbers);
+            // 양수가 아닌 경우
+            if (num <= 0) {
+                throw new IllegalArgumentException("양수만 가능합니다.");
+            }
+            return num;
+        } catch (NumberFormatException e) {
+            // 숫자가 아닌 문자가 오는 경우(= 지정된 구분자외에 다른 구분자가 포함되어 있는 경우)
+            throw new IllegalArgumentException("지정된 구분자외에 다른 구분자는 사용할 수 없습니다.");
+        }
+    }
+
+    private static void isCustomSeparatorDot(String customSeparator) {
+        if (customSeparator.equals(".")) {
+            throw new IllegalArgumentException("커스텀 구분자로 마침표(.)은 사용할 수 없습니다.");
+        }
+    }
+
+    private static void isCustomSeparatorNumber(String customSeparator) {
+        try {
+            double value = Double.parseDouble(customSeparator);
+            // 변환이 성공하면 예외 발생
+            throw new IllegalArgumentException("커스텀 구분자로 숫자는 사용할 수 없습니다.");
+        } catch (NumberFormatException ignored) {
+        }
     }
 }
