b2bceef885e9774ab821597caee5fe9456d3985c
jhk01007 <jhk001007@naver.com>
2025-10-17 04:29:39 +0900

    refactor: 클래스 구조 변경
    - StringSumCalculator의 로직을 DelimiterParser와 NumberParser로 분리
    - DelimiterParser: 문자열에서 구분자를 추출하는 클래스
    - NumberParser: 문자열에서 숫자를 추출하는 클래스

---
+++ src/main/java/calculator/DelimiterParser.java
@@ -0,0 +1,64 @@
+package calculator;
+
+import java.util.Optional;
+
+/**
+ * 문자열에서 구분자를 추출하는 클래스
+ */
+public class DelimiterParser {
+
+    private static final String CUSTOM_DELIMITER_REGEX = "^//[^\\\\n]+\\\\n.*"; // 커스텀 구분자 정규표현식
+
+    /**
+     * 커스텀 구분자를 추출하는 메서드
+     * @param str - 추출할 문자열
+     * @return 커스텀 구분자
+     */
+    public Optional<String> parseCustomDelimiter(String str) {
+
+        // 커스텀 구분자를 추가하는 입력값이라면 (정규 표현식 활용)
+        if (str.matches(CUSTOM_DELIMITER_REGEX)) {
+            String customDelimiter = str.substring(2, str.indexOf("\\n")); // 커스텀 구분자 추출
+
+            validateCustomDelimiterIsNotNumber(customDelimiter); // 커스텀 구분자가 숫자인지 검증
+            validateCustomDelimiterIsNotDot(customDelimiter); // 커스텀 구분자가 마침표(.)인지 검증
+
+            return Optional.of(customDelimiter);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * 문자열로부터 커스텀 구분자 접두사를 제거하는 메서드
+     * @param str 제거할 문자열
+     * @return 제거된 결과
+     */
+    public String removeCustomDelimiterPrefix(String str) {
+        return str.matches(CUSTOM_DELIMITER_REGEX) ?
+                str.substring(str.indexOf("\\n") + 2) : str;
+    }
+
+    /**
+     * 커스텀 구분자가 마침표(.)이 아닌지 검증
+     * @param customDelimiter - 커스텀 구분자
+     */
+    private void validateCustomDelimiterIsNotDot(String customDelimiter) {
+        if (customDelimiter.equals(".")) {
+            throw new IllegalArgumentException("커스텀 구분자로 마침표(.)은 사용할 수 없습니다.");
+        }
+    }
+
+    /**
+     * 커스텀 구분자가 숫자가 아닌지 검증
+     * @param customDelimiter
+     */
+    private void validateCustomDelimiterIsNotNumber(String customDelimiter) {
+        try {
+            Double.parseDouble(customDelimiter);
+            // 변환이 성공하면 예외 발생
+            throw new IllegalArgumentException("커스텀 구분자로 숫자는 사용할 수 없습니다.");
+        } catch (NumberFormatException ignored) {
+        }
+    }
+}
---
+++ src/main/java/calculator/NumberParser.java
@@ -0,0 +1,62 @@
+package calculator;
+
+import java.util.*;
+
+/**
+ * 문자열에서 숫자를 추출하는 클래스
+ */
+public class NumberParser {
+    /**
+     * 문자열로부터 구분자를 제거하고 숫자만 반환
+     * @param str - 계산할 문자열
+     * @param delimiters - 구분자 셋
+     * @return - 추출된 숫자 리스트
+     */
+    public List<Double> parseNumbers(String str, Set<String> delimiters) {
+
+        // 각 구분자를 통일
+        for (String delimiter : delimiters) {
+            str = str.replace(delimiter, " ");
+        }
+
+        // 숫자들만 뽑아냄
+        String[] parsedNumbers = str.split(" ");
+
+        // 뽑아낸 숫자들을 Double로 형 변환
+        List<Double> convertedNumbers = new ArrayList<>();
+        for (int i = 0; i < parsedNumbers.length; i++) {
+            convertedNumbers.add(convertStringToNumber(parsedNumbers[i]));
+        }
+
+        return convertedNumbers;
+    }
+
+    /**
+     * 문자열을 숫자로 변환
+     * @param number - 변환할 문자열
+     * @return - 변환된 숫자
+     */
+    private double convertStringToNumber(String number) {
+        try {
+            double num = Double.parseDouble(number);
+
+            // 양수인지 검증
+            validatePositiveNumber(num);
+
+            return num;
+        } catch (NumberFormatException e) {
+            // 숫자가 아닌 문자가 오는 경우(= 지정된 구분자외에 다른 구분자가 포함되어 있는 경우)
+            throw new IllegalArgumentException("입력값이 잘못됐거나 지정된 구분자외에 다른 구분자는 사용할 수 없습니다.");
+        }
+    }
+
+    /**
+     * 해당 숫자가 양수인지 검증
+     * @param num - 검증할 숫자
+     */
+    private void validatePositiveNumber(double num) {
+        if (num <= 0) {
+            throw new IllegalArgumentException("양수만 가능합니다.");
+        }
+    }
+}
